b9e85dee5a3b714899d0fd72e2fa12fbe6cca388
Miguel <gannasong@gmail.com>
2020-12-13 00:18:53 +0800

    Add `CoreDataFeedStore` data model

---
+++ CoreDataModel/ManagedCache.swift
@@ -0,0 +1,24 @@
+//
+//  ManagedCache.swift
+//  Tests
+//
+//  Created by SUNG HAO LIN on 2020/12/12.
+//  Copyright © 2020 Essential Developer. All rights reserved.
+//
+
+import CoreData
+
+@objc(ManagedCache)
+internal class ManagedCache: NSManagedObject {
+	@NSManaged internal var timestamp: Date
+	@NSManaged internal var feed: NSOrderedSet
+}
+
+@objc(ManagedFeedImage)
+internal class ManagedFeedImage: NSManagedObject {
+	@NSManaged internal var id: UUID
+	@NSManaged internal var imageDescription: String?
+	@NSManaged internal var location: String?
+	@NSManaged internal var url: URL
+	@NSManaged internal var cache: ManagedCache
+}
