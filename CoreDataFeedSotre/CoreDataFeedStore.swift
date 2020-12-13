b7ae6ba5b695d989d72d8bf9af9548a31986ab5a
Miguel <gannasong@gmail.com>
2020-12-14 00:28:15 +0800

    Add CoreData infrastructure

--- CoreDataFeedSotre/CoreDataFeedStore.swift
+++ CoreDataFeedSotre/CoreDataFeedStore.swift
@@ -6,11 +6,17 @@
 //  Copyright © 2020 Essential Developer. All rights reserved.
 //
 
-import Foundation
+import CoreData
 
-public class CoreDataFeedStore: FeedStore {
+public final class CoreDataFeedStore: FeedStore {
+	private let container: NSPersistentContainer
+	private let context: NSManagedObjectContext
+	private let modelName = "FeedStore"
 
-	public init() {}
+	public init(storeURL: URL, bundle: Bundle = .main) throws {
+		container = try NSPersistentContainer.load(modelName: modelName, url: storeURL, in: bundle)
+		context = container.newBackgroundContext()
+	}
 
 	public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
 	}
@@ -21,4 +27,50 @@ public class CoreDataFeedStore: FeedStore {
 	public func retrieve(completion: @escaping RetrievalCompletion) {
 		completion(.empty)
 	}
+}
+
+private extension NSPersistentContainer {
+	enum LoadingError: Swift.Error {
+		case modelNotFound
+		case failedToLoadPersistentStores(Swift.Error)
+	}
+
+	static func load(modelName name: String, url: URL, in bundle: Bundle) throws -> NSPersistentContainer {
+		guard let model = NSManagedObjectModel.with(name: name, in: bundle) else {
+			throw LoadingError.modelNotFound
+		}
+
+		let description = NSPersistentStoreDescription(url: url)
+		let container = NSPersistentContainer(name: name, managedObjectModel: model)
+		container.persistentStoreDescriptions = [description]
+
+		var loadError: Swift.Error?
+		container.loadPersistentStores { loadError = $1 }
+		try loadError.map { throw LoadingError.failedToLoadPersistentStores($0) }
+
+		return container
+	}
+}
+
+private extension NSManagedObjectModel {
+	static func with(name: String, in bundle: Bundle) -> NSManagedObjectModel? {
+		return bundle
+			.url(forResource: name, withExtension: "momd")
+			.flatMap { NSManagedObjectModel(contentsOf: $0) }
+	}
+}
+
+@objc(ManagedCache)
+private class ManagedCache: NSManagedObject {
+	@NSManaged var timestamp: Date
+	@NSManaged var feed: NSOrderedSet
+}
+
+@objc(ManagedFeedImage)
+private class ManagedFeedImage: NSManagedObject {
+	@NSManaged var id: UUID
+	@NSManaged var imageDescription: String?
+	@NSManaged var location: String?
+	@NSManaged var url: URL
+	@NSManaged var cache: ManagedCache
 }
